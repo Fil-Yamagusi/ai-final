c90bc1e60acda622073db761f06295f081e799bd
BuildTools <unconfigured@null.spigotmc.org>
2024-04-19 23:19:03 +0300

    Начало длинного пути

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3.12
+# -*- coding: utf-8 -*-
+"""2024-04-20 Fil - Future code Yandex.Practicum
+Final AI-bot: GPT, STT, TTS
+README.md for more
+
+Fil FC Personal assistant
+@fil_fc_ai_pa
+https://t.me/fil_fc_ai_pa_bot
+"""
+__version__ = '0.1'
+__author__ = 'Firip Yamagusi'
+
+
+
