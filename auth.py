50cb5b04b219eab60ce00c78bcd0ba24bf5aceed
BuildTools <unconfigured@null.spigotmc.org>
2024-05-17 23:45:39 +0300

    auth.py теперь не секретный. Загружаю для клонирования на ВМ

---
+++ auth.py
@@ -0,0 +1,12 @@
+#
+# Telegram bot
+_TB = {}
+_TB['TOKEN'] = ''
+_TB['BOT_NAME'] = 'ИИ-помощник для планов на лето'
+_TB['BOT_USERNAME'] = 'fil_fc_ai_pa_bot'
+
+# Yandex API
+_YANDEX = {}
+_YANDEX['GPT_MODEL'] = 'yandexgpt-lite'
+_YANDEX['FOLDER_ID'] = ''
+_YANDEX['IAM_TOKEN'] = ''
